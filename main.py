f80c1da7519fb3f29386e12a3175f2bd7afe0f44
oscarm24 <oscarmontoyasr001@gmail.com>
2025-08-21 16:14:01 -0600

    Despliegue

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
+import uvicorn
 from bd.database import engine, Base
 from routers.movie import routerMovie
 from routers.users import login_user
+import os
 
 app = FastAPI( 
     title='Aprendiendo FastAPI',
@@ -22,3 +24,6 @@ Base.metadata.create_all(bind=engine)
 def read_root():
     return HTMLResponse('<h2>Hola Mundo!</h2>')
 
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT",8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
